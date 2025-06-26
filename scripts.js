7762900fae921a2f3288c8694f526e8e8aa88000
Icarotika <tikaucaro@gmail.com>
2025-06-25 22:47:05 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Script para enviar o formulário de contato
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mensagem enviada! Em breve entrarei em contato.');
-});
